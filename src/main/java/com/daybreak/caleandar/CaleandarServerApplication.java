2b0ef22cbfa99c4f8cf8a6a0bfee46d4050756ca
tnatna0801 <min120926@gmail.com>
2022-02-04 00:43:30 +0900

    Caleandar project 생성

---
+++ src/main/java/com/daybreak/caleandar/CaleandarServerApplication.java
@@ -0,0 +1,13 @@
+package com.daybreak.caleandar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaleandarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaleandarServerApplication.class, args);
+	}
+
+}
