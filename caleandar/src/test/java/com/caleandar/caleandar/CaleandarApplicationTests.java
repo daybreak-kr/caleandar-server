efaee8e1453e8410f1881356e73a50b29999df6c
tnatna0801 <min120926@gmail.com>
2022-02-03 23:49:46 +0900

    Caleandar project 생성

---
+++ caleandar/src/test/java/com/caleandar/caleandar/CaleandarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caleandar.caleandar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaleandarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
