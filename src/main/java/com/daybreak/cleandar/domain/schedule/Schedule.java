f1c345ef07707c90c5ef908969156b1e2c3a662e
tnatna0801 <min120926@gmail.com>
2022-02-06 23:26:54 +0900

    Schedule Entity 생성

---
+++ src/main/java/com/daybreak/cleandar/domain/schedule/Schedule.java
@@ -0,0 +1,52 @@
+package com.daybreak.cleandar.domain.schedule;
+
+import com.daybreak.cleandar.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "schedule")
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime start;
+
+    private LocalDateTime end;
+
+    private String title;
+
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    @Builder
+    public Schedule(LocalDateTime start, LocalDateTime end,
+                    String title, String description,
+                    User user){
+        this.start = start;
+        this.end = end;
+        this.title = title;
+        this.description = description;
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/daybreak/cleandar/domain/schedule/ScheduleDto.java
@@ -0,0 +1,53 @@
+package com.daybreak.cleandar.domain.schedule;
+
+import com.daybreak.cleandar.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ScheduleDto {
+
+    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Request{
+        private String start;
+        private String end;
+        private String title;
+        private String description;
+
+        public Schedule toEntity(User user) {
+            return Schedule.builder()
+                    .start(LocalDateTime.parse(start, formatter))
+                    .end(LocalDateTime.parse(end, formatter))
+                    .title(title)
+                    .description(description)
+                    .user(user)
+                    .build();
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response {
+        private Long id;
+        private String start;
+        private String end;
+        private String title;
+        private String description;
+
+        public Response(Schedule schedule){
+            id = schedule.getId();
+            start = schedule.getStart().format(formatter);
+            end = schedule.getEnd().format(formatter);
+            title = schedule.getTitle();
+            description = schedule.getDescription();
+        }
+    }
+}
