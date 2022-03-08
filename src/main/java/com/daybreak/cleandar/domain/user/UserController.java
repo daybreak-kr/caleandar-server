ef403c4c2f50a628a4be71466c12fd3e5f201930
JoungSik <tjstlr2010@gmail.com>
2022-03-08 20:44:48 +0900

    add: 회원가입, 로그인 form 구현

--- src/main/java/com/daybreak/cleandar/domain/user/UserController.java
+++ src/main/java/com/daybreak/cleandar/domain/user/UserController.java
@@ -1,30 +1,42 @@
 package com.daybreak.cleandar.domain.user;
 
 import com.daybreak.cleandar.security.UserPrincipal;
+import com.daybreak.cleandar.security.UserPrincipalDetailsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping(path = "/api/users")
-@CrossOrigin(origins = "*")
+@Controller
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    private final UserPrincipalDetailsService userService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserDto.Response createUser(@RequestBody UserDto.Request request) {
-        User user = userService.create(request);
-        return new UserDto.Response(user);
+    @GetMapping("/login")
+    public String loginForm() {
+        return "users/login";
     }
 
-    @PutMapping
-    @ResponseStatus(HttpStatus.OK)
-    public UserDto.Response updateUser(@AuthenticationPrincipal UserPrincipal userPrincipal,
-                                       @RequestBody UserDto.Request request) {
-        return userService.update(userPrincipal.getUser(), request);
+    @GetMapping("/register")
+    public String registerForm() {
+        return "users/new";
     }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute UserDto.Request request) {
+        userService.create(request);
+        return "redirect:/";
+    }
+
+    // TODO 미사용 코드
+    // @GetMapping("/users")
+    // public ModelAndView updateForm(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+    //     ModelAndView mav = new ModelAndView("user");
+    //     mav.addObject("userDto", new UserDto.Response(userPrincipal.getUser()));
+    //     return mav;
+    // }
 }
--- src/main/java/com/daybreak/cleandar/security/UserPrincipalDetailsService.java
+++ src/main/java/com/daybreak/cleandar/security/UserPrincipalDetailsService.java
@@ -1,11 +1,13 @@
 package com.daybreak.cleandar.security;
 
 import com.daybreak.cleandar.domain.user.User;
+import com.daybreak.cleandar.domain.user.UserDto;
 import com.daybreak.cleandar.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +16,17 @@ public class UserPrincipalDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    public User create(UserDto.Request request) {
+        return userRepository.save(User.builder()
+                .email(request.getEmail())
+                .password(new BCryptPasswordEncoder().encode(request.getPassword()))
+                .name(request.getName())
+                .build());
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findUserByEmail(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
         return new UserPrincipal(user);
     }
 
